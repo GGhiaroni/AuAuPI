673feb1c849849bcb711c3ba8f783986b625f333
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-01 11:27:26 -0300

    first commit

---
+++ src/main/java/com/GabrielTiziano/AuAuPI/AuAuPiApplication.java
@@ -0,0 +1,13 @@
+package com.GabrielTiziano.AuAuPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuAuPiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuAuPiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/GabrielTiziano/AuAuPI/AuAuPiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GabrielTiziano.AuAuPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuAuPiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
