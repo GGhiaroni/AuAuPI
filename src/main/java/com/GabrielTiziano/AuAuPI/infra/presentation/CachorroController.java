96b8d52d42c49a7117ca291a179d39360cbf912e
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-07 14:23:28 -0300

    feat: Inicia a criacao do primeiro endpoint para criacao de Cachorro

--- src/main/java/com/GabrielTiziano/AuAuPI/infra/presentation/CachorroController.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/infra/presentation/CachorroController.java
@@ -1,14 +1,30 @@
 package com.GabrielTiziano.AuAuPI.infra.presentation;
 
+import com.GabrielTiziano.AuAuPI.core.entities.Cachorro;
+import com.GabrielTiziano.AuAuPI.core.entities.Tutor;
+import com.GabrielTiziano.AuAuPI.core.usecases.cachorro.CriarCachorroCase;
+import com.GabrielTiziano.AuAuPI.core.usecases.tutor.BuscarTutorPorIdCase;
+import com.GabrielTiziano.AuAuPI.infra.dto.request.CriarCachorroRequest;
+import com.GabrielTiziano.AuAuPI.infra.dto.response.CachorroResponse;
+import com.GabrielTiziano.AuAuPI.infra.dto.response.TutorResumoResponse;
+import com.GabrielTiziano.AuAuPI.infra.mapper.CachorroMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/cachorro")
+@RequiredArgsConstructor
 public class CachorroController {
+    private final CriarCachorroCase criarCachorroCase;
+    private final BuscarTutorPorIdCase buscarTutorPorIdCase;
+
     @PostMapping
-    public String criarCachorro(){
-        return "Cachorro criado.";
+    public CachorroResponse criarCachorro(@RequestBody CriarCachorroRequest dto){
+        Cachorro cachorroCriado = criarCachorroCase.execute(CachorroMapper.toDomain(dto));
+        Tutor tutor = buscarTutorPorIdCase.execute(cachorroCriado.idTutor());
+        return CachorroMapper.toResponse(cachorroCriado, new TutorResumoResponse(tutor.id(), tutor.nome(), tutor.telefone()));
     }
 }
