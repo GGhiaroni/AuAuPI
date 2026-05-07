342075baf13d07a65dedcd96ceddc51e001da763
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-07 16:44:03 -0300

    feat: Adiciona os métodos iniciais dentro dos Gateways de Cachorro, Tutor e Reserva

--- src/main/java/com/GabrielTiziano/AuAuPI/core/gateway/CachorroGateway.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/core/gateway/CachorroGateway.java
@@ -1,4 +1,15 @@
 package com.GabrielTiziano.AuAuPI.core.gateway;
 
+import com.GabrielTiziano.AuAuPI.core.entities.Cachorro;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface CachorroGateway {
+    Cachorro save(Cachorro cachorro);
+    Optional<Cachorro> findById(Long id);
+    List<Cachorro> findAll();
+    List<Cachorro> findByIdTutor(Long idTutor);
+    void deleteById(Long id);
+    boolean existsById(Long id);
 }
