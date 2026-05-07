7fb3b4b43d1f4f004e78035213bb45ef18496343
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-07 14:37:20 -0300

    feat: Cria a estrutura dos demais usecases de Tutor

---
+++ src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/tutor/DeletarTutorPorIdCaseImpl.java
@@ -0,0 +1,8 @@
+package com.GabrielTiziano.AuAuPI.core.usecases.tutor;
+
+public class DeletarTutorPorIdCaseImpl implements DeletarTutorPorIdCase{
+    @Override
+    public void execute(Long id) {
+
+    }
+}
---
+++ src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/tutor/ListarTutoresCase.java
@@ -0,0 +1,9 @@
+package com.GabrielTiziano.AuAuPI.core.usecases.tutor;
+
+import com.GabrielTiziano.AuAuPI.core.entities.Tutor;
+
+import java.util.List;
+
+public interface ListarTutoresCase {
+   public List<Tutor> execute();
+}
