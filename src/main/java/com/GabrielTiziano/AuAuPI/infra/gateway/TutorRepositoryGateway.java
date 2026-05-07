ccc8904169f85587231f3f42a8d69d84f45704bc
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-07 17:12:52 -0300

    feat: Cria a conexão com o banco de dados e os métodos responsáveis por salvar, encontrar por id, listar, deletar e encontrar por cpf

--- src/main/java/com/GabrielTiziano/AuAuPI/infra/gateway/TutorRepositoryGateway.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/infra/gateway/TutorRepositoryGateway.java
@@ -2,38 +2,51 @@ package com.GabrielTiziano.AuAuPI.infra.gateway;
 
 import com.GabrielTiziano.AuAuPI.core.entities.Tutor;
 import com.GabrielTiziano.AuAuPI.core.gateway.TutorGateway;
+import com.GabrielTiziano.AuAuPI.infra.mapper.TutorMapper;
+import com.GabrielTiziano.AuAuPI.infra.persistence.TutorEntity;
+import com.GabrielTiziano.AuAuPI.infra.persistence.TutorRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Optional;
 
+@Component
+@RequiredArgsConstructor
 public class TutorRepositoryGateway implements TutorGateway {
+    private final TutorRepository tutorRepository;
+
     @Override
     public Tutor save(Tutor tutor) {
-        return null;
+       TutorEntity entitySalva = tutorRepository.save(TutorMapper.toEntity(tutor));
+       return TutorMapper.toDomain(entitySalva);
     }
 
     @Override
     public Optional<Tutor> findById(Long id) {
-        return Optional.empty();
+        return tutorRepository.findById(id)
+                .map(TutorMapper::toDomain);
     }
 
     @Override
     public List<Tutor> findAll() {
-        return List.of();
+        return tutorRepository.findAll()
+                .stream().map(TutorMapper::toDomain)
+                .toList();
     }
 
     @Override
     public void deleteById(Long id) {
-
+        tutorRepository.deleteById(id);
     }
 
     @Override
     public boolean existsById(Long id) {
-        return false;
+        return tutorRepository.existsById(id);
     }
 
     @Override
     public boolean existsByCpf(String cpf) {
-        return false;
+        return tutorRepository.existsByCpf(cpf);
     }
 }
