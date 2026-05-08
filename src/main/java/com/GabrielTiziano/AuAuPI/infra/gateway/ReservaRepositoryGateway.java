2b5a4cbfe413b604c59f7a845a0bcbdc04b609a2
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-08 10:38:54 -0300

    feat: Implementa os métodos do ReservaRepositoryGateway

--- src/main/java/com/GabrielTiziano/AuAuPI/infra/gateway/ReservaRepositoryGateway.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/infra/gateway/ReservaRepositoryGateway.java
@@ -3,49 +3,76 @@ package com.GabrielTiziano.AuAuPI.infra.gateway;
 import com.GabrielTiziano.AuAuPI.core.entities.Reserva;
 import com.GabrielTiziano.AuAuPI.core.enums.StatusReserva;
 import com.GabrielTiziano.AuAuPI.core.gateway.ReservaGateway;
+import com.GabrielTiziano.AuAuPI.infra.mapper.ReservaMapper;
+import com.GabrielTiziano.AuAuPI.infra.persistence.*;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
 
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+@Component
+@RequiredArgsConstructor
 public class ReservaRepositoryGateway implements ReservaGateway {
+    private final ReservaRepository reservaRepository;
+    private final CachorroRepository cachorroRepository;
+
     @Override
     public Reserva save(Reserva reserva) {
-        return null;
+        CachorroEntity cachorroEntity = cachorroRepository.findById(reserva.idCachorro())
+                .orElseThrow(() -> new IllegalStateException(
+                        "Cachorro inexistente ao salvar reserva: " + reserva.idCachorro()
+                ));
+
+        return ReservaMapper.toDomain(
+                reservaRepository.save(
+                        ReservaMapper.toEntity(
+                                reserva, cachorroEntity
+                        )));
     }
 
     @Override
     public Optional<Reserva> findById(Long id) {
-        return Optional.empty();
+        return reservaRepository.findById(id)
+                .map(ReservaMapper::toDomain);
     }
 
     @Override
     public List<Reserva> findAll() {
-        return List.of();
+        return reservaRepository.findAll()
+                .stream().map(ReservaMapper::toDomain)
+                .toList();
     }
 
     @Override
     public List<Reserva> findByIdCachorro(Long idCachorro) {
-        return List.of();
+        return reservaRepository.findByCachorroId(idCachorro)
+                .stream().map(ReservaMapper::toDomain)
+                .toList();
     }
 
     @Override
     public void deleteById(Long id) {
-
+        reservaRepository.deleteById(id);
     }
 
     @Override
     public boolean existsById(Long id) {
-        return false;
+        return reservaRepository.existsById(id);
     }
 
     @Override
     public List<Reserva> findByStatus(StatusReserva status) {
-        return List.of();
+        return reservaRepository.findByStatus(status)
+                .stream().map(ReservaMapper::toDomain)
+                .toList();
     }
 
     @Override
     public List<Reserva> findByPeriodo(LocalDate inicio, LocalDate fim) {
-        return List.of();
+        return reservaRepository.findByPeriodo(inicio, fim)
+                .stream().map(ReservaMapper::toDomain)
+                .toList();
     }
 }
--- src/main/java/com/GabrielTiziano/AuAuPI/infra/persistence/ReservaRepository.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/infra/persistence/ReservaRepository.java
@@ -1,6 +1,23 @@
 package com.GabrielTiziano.AuAuPI.infra.persistence;
 
+import com.GabrielTiziano.AuAuPI.core.enums.StatusReserva;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface ReservaRepository extends JpaRepository<ReservaEntity, Long> {
+    List<ReservaEntity> findByCachorroId(Long id);
+    List<ReservaEntity> findByStatus(StatusReserva status);
+    @Query("""
+    SELECT r FROM ReservaEntity r
+    WHERE r.dataCheckin <= :fim
+      AND r.dataCheckout >= :inicio
+""")
+    List<ReservaEntity> findByPeriodo(
+            @Param("inicio") LocalDate inicio,
+            @Param("fim") LocalDate fim
+    );
 }
