dce8fbb0dce99bdb0676d2f632e274d7f33fd0ec
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-08 14:41:32 -0300

    feat: Faz a injeção de dependência de CachorroGateway no CriarCachorroCaseImpl

--- src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/cachorro/CriarCachorroCaseImpl.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/cachorro/CriarCachorroCaseImpl.java
@@ -1,8 +1,13 @@
 package com.GabrielTiziano.AuAuPI.core.usecases.cachorro;
 
 import com.GabrielTiziano.AuAuPI.core.entities.Cachorro;
+import com.GabrielTiziano.AuAuPI.core.gateway.CachorroGateway;
+import lombok.RequiredArgsConstructor;
 
+@RequiredArgsConstructor
 public class CriarCachorroCaseImpl implements CriarCachorroCase{
+    private final CachorroGateway cachorroGateway;
+
     @Override
     public Cachorro execute(Cachorro cachorro) {
         return null;
