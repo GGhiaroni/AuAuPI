2c9a23f8daca7be68aede4e03b394349a285eb90
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-08 11:13:53 -0300

    feat: Cria o método findByTutorId no CachorroRepository

--- src/main/java/com/GabrielTiziano/AuAuPI/infra/persistence/CachorroRepository.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/infra/persistence/CachorroRepository.java
@@ -2,5 +2,8 @@ package com.GabrielTiziano.AuAuPI.infra.persistence;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CachorroRepository extends JpaRepository<CachorroEntity, Long> {
+    List<CachorroEntity> findByTutorId(Long id);
 }
