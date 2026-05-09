ace3e7c78443f9bc70f31c9930090266501c5987
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-09 17:00:53 -0300

    feat: Implementa corretamente os endpoints de 'criar tutor' e 'criar cachorro'

--- src/main/java/com/GabrielTiziano/AuAuPI/infra/presentation/TutorController.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/infra/presentation/TutorController.java
@@ -1,14 +1,27 @@
 package com.GabrielTiziano.AuAuPI.infra.presentation;
 
+import com.GabrielTiziano.AuAuPI.core.entities.Tutor;
+import com.GabrielTiziano.AuAuPI.core.usecases.tutor.CriarTutorCase;
+import com.GabrielTiziano.AuAuPI.infra.dto.request.CriarTutorRequest;
+import com.GabrielTiziano.AuAuPI.infra.dto.response.TutorResponse;
+import com.GabrielTiziano.AuAuPI.infra.mapper.TutorMapper;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/tutor")
+@RequestMapping("/tutores")
+@RequiredArgsConstructor
 public class TutorController {
+    private final CriarTutorCase criarTutorCase;
     @PostMapping
-    public String criarTutor(){
-        return "Tutor criado.";
+    public ResponseEntity<TutorResponse> criarTutor(@Valid @RequestBody CriarTutorRequest dto){
+        Tutor tutorCriado = criarTutorCase.execute(TutorMapper.toDomain(dto));
+        return ResponseEntity.status(HttpStatus.CREATED).body(TutorMapper.toResponse(tutorCriado));
     }
 }
