46f3531c3ee791289dc0fa77cee5d8ed33f92459
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-09 11:34:04 -0300

    feat: Cria o fluxo dos usecases responsáveis por criar reserva e criar tutor

--- src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/tutor/CriarTutorCase.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/tutor/CriarTutorCase.java
@@ -3,5 +3,5 @@ package com.GabrielTiziano.AuAuPI.core.usecases.tutor;
 import com.GabrielTiziano.AuAuPI.core.entities.Tutor;
 
 public interface CriarTutorCase {
-    public Tutor execute();
+    public Tutor execute(Tutor tutor);
 }
--- src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/tutor/CriarTutorCaseImpl.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/tutor/CriarTutorCaseImpl.java
@@ -1,10 +1,35 @@
 package com.GabrielTiziano.AuAuPI.core.usecases.tutor;
 
 import com.GabrielTiziano.AuAuPI.core.entities.Tutor;
+import com.GabrielTiziano.AuAuPI.core.gateway.TutorGateway;
+
+import java.time.LocalDateTime;
 
 public class CriarTutorCaseImpl implements  CriarTutorCase{
+    private final TutorGateway tutorGateway;
+
+    public CriarTutorCaseImpl(TutorGateway tutorGateway) {
+        this.tutorGateway = tutorGateway;
+    }
+
     @Override
-    public Tutor execute() {
-        return null;
+    public Tutor execute(Tutor tutor) {
+        if(tutorGateway.existsByCpf(tutor.cpf())){
+            throw new IllegalArgumentException(
+                    "Tutor com CPF: " + tutor.cpf() + " já cadastrado."
+            );
+        }
+
+        Tutor tutorASalvar = new Tutor(
+                null,
+                tutor.nome(),
+                tutor.cpf(),
+                tutor.email(),
+                tutor.endereco(),
+                tutor.telefone(),
+                LocalDateTime.now()
+        );
+
+        return tutorGateway.save(tutorASalvar);
     }
 }
