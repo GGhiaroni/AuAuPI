2c0891092f9d2713ce631ba8941721559e50b7a0
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-10 20:55:44 -0300

    feat: Implementa regex para padronizar o CPF antes de salvar o usuário no banco

--- src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/tutor/AtualizarTutorPorIdCaseImpl.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/tutor/AtualizarTutorPorIdCaseImpl.java
@@ -16,21 +16,23 @@ public class AtualizarTutorPorIdCaseImpl implements AtualizarTutorPorIdCase{
     public Tutor execute(Long id, Tutor tutor) {
         Optional<Tutor> tutorExistente = tutorGateway.findById(id);
 
+        String cpfNormalizado = tutor.cpf().replaceAll("[^0-9]", "");
+
         if(tutorExistente.isEmpty()){
             throw new IllegalArgumentException("Não foi possível localizar um tutor de id " + id + ".");
         }
 
-        if (!tutorExistente.get().cpf().equals(tutor.cpf())
-                && tutorGateway.existsByCpf(tutor.cpf())) {
+        if (!tutorExistente.get().cpf().equals(cpfNormalizado)
+                && tutorGateway.existsByCpf(cpfNormalizado)) {
             throw new IllegalArgumentException(
-                    "CPF " + tutor.cpf() + " já está cadastrado por outro tutor."
+                    "CPF " + cpfNormalizado + " já está cadastrado por outro tutor."
             );
         }
 
         Tutor tutorASalvar = new Tutor(
           tutorExistente.get().id(),
                 tutor.nome(),
-                tutor.cpf(),
+                cpfNormalizado,
                 tutor.email(),
                 tutor.endereco(),
                 tutor.telefone(),
