0974c3e9e9c4ebe396d4295edb3b2ae74fe8a75d
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-10 19:56:12 -0300

    feat: Cria os endpoints responsáveis pela atualizacao, delecao, listagem de tutores e busca por um tutor em específico

--- src/main/java/com/GabrielTiziano/AuAuPI/infra/presentation/TutorController.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/infra/presentation/TutorController.java
@@ -1,7 +1,7 @@
 package com.GabrielTiziano.AuAuPI.infra.presentation;
 
 import com.GabrielTiziano.AuAuPI.core.entities.Tutor;
-import com.GabrielTiziano.AuAuPI.core.usecases.tutor.CriarTutorCase;
+import com.GabrielTiziano.AuAuPI.core.usecases.tutor.*;
 import com.GabrielTiziano.AuAuPI.infra.dto.request.CriarTutorRequest;
 import com.GabrielTiziano.AuAuPI.infra.dto.response.TutorResponse;
 import com.GabrielTiziano.AuAuPI.infra.mapper.TutorMapper;
@@ -9,19 +9,50 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/tutores")
 @RequiredArgsConstructor
 public class TutorController {
+    private final ListarTutoresCase listarTutoresCase;
+    private final BuscarTutorPorIdCase buscarTutorPorIdCase;
     private final CriarTutorCase criarTutorCase;
+    private final AtualizarTutorPorIdCase atualizarTutorPorIdCase;
+    private final DeletarTutorPorIdCase deletarTutorPorIdCase;
+
+    @GetMapping
+    public ResponseEntity<List<TutorResponse>> listarTutores() {
+        List<Tutor> tutorList = listarTutoresCase.execute();
+        List<TutorResponse> tutorResponseList = tutorList.stream()
+                .map(TutorMapper::toResponse)
+                .toList();
+
+        return ResponseEntity.ok(tutorResponseList);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<TutorResponse> buscarTutorPorId(@PathVariable Long id){
+        return ResponseEntity.ok(TutorMapper.toResponse(buscarTutorPorIdCase.execute(id)));
+    }
+
     @PostMapping
-    public ResponseEntity<TutorResponse> criarTutor(@Valid @RequestBody CriarTutorRequest dto){
+    public ResponseEntity<TutorResponse> criarTutor(@Valid @RequestBody CriarTutorRequest dto) {
         Tutor tutorCriado = criarTutorCase.execute(TutorMapper.toDomain(dto));
         return ResponseEntity.status(HttpStatus.CREATED).body(TutorMapper.toResponse(tutorCriado));
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<TutorResponse> atualizarTutor(@PathVariable Long id, @Valid @RequestBody CriarTutorRequest dto){
+        Tutor tutorAtualizado = atualizarTutorPorIdCase.execute(id, TutorMapper.toDomain(dto));
+        return ResponseEntity.ok(TutorMapper.toResponse(tutorAtualizado));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarTutor(@PathVariable Long id) {
+        deletarTutorPorIdCase.execute(id);
+        return ResponseEntity.noContent().build();
     }
 }
