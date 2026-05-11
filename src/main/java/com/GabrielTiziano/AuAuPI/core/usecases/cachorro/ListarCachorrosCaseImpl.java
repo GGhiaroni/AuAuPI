82a81a4a1419db66ce1bc512b57314b223f35352
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-11 12:03:16 -0300

    feat: Implementa o usecase de listar cachorros

--- src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/cachorro/ListarCachorrosCaseImpl.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/cachorro/ListarCachorrosCaseImpl.java
@@ -1,4 +1,19 @@
 package com.GabrielTiziano.AuAuPI.core.usecases.cachorro;
 
+import com.GabrielTiziano.AuAuPI.core.entities.Cachorro;
+import com.GabrielTiziano.AuAuPI.core.gateway.CachorroGateway;
+
+import java.util.List;
+
 public class ListarCachorrosCaseImpl implements ListarCachorrosCase{
+    private final CachorroGateway cachorroGateway;
+
+    public ListarCachorrosCaseImpl(CachorroGateway cachorroGateway) {
+        this.cachorroGateway = cachorroGateway;
+    }
+
+    @Override
+    public List<Cachorro> execute() {
+       return cachorroGateway.findAll();
+    }
 }
