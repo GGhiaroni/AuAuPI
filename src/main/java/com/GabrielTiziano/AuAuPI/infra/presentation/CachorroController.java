a9c0259922a95507c5c73cb3930f526f4ccc9700
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-11 12:02:37 -0300

    feat: Cria o endpoint responsável por listar todos os cachorros

--- src/main/java/com/GabrielTiziano/AuAuPI/infra/presentation/CachorroController.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/infra/presentation/CachorroController.java
@@ -3,6 +3,7 @@ package com.GabrielTiziano.AuAuPI.infra.presentation;
 import com.GabrielTiziano.AuAuPI.core.entities.Cachorro;
 import com.GabrielTiziano.AuAuPI.core.entities.Tutor;
 import com.GabrielTiziano.AuAuPI.core.usecases.cachorro.CriarCachorroCase;
+import com.GabrielTiziano.AuAuPI.core.usecases.cachorro.ListarCachorrosCase;
 import com.GabrielTiziano.AuAuPI.core.usecases.tutor.BuscarTutorPorIdCase;
 import com.GabrielTiziano.AuAuPI.infra.dto.request.CriarCachorroRequest;
 import com.GabrielTiziano.AuAuPI.infra.dto.response.CachorroResponse;
@@ -12,10 +13,11 @@ import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static java.util.stream.Collectors.toList;
 
 @RestController
 @RequestMapping("/cachorros")
@@ -23,6 +25,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class CachorroController {
     private final CriarCachorroCase criarCachorroCase;
     private final BuscarTutorPorIdCase buscarTutorPorIdCase;
+    private final ListarCachorrosCase listarCachorrosCase;
+
+    @GetMapping
+    public ResponseEntity<List<CachorroResponse>> listarCachorros() {
+        List<Cachorro> cachorroList = listarCachorrosCase.execute();
+
+        List<CachorroResponse> cachorroResponseList = cachorroList.stream()
+                .map(cachorro ->
+                        CachorroMapper.toResponse(cachorro,
+                                TutorMapper.toResumoResponse(buscarTutorPorIdCase.execute((cachorro.idTutor())))))
+                .toList();
+
+        return ResponseEntity.ok(cachorroResponseList);
+    }
 
     @PostMapping
     public ResponseEntity<CachorroResponse> criarCachorro(@Valid @RequestBody CriarCachorroRequest dto) {
