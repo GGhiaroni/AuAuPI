6018c089111ad8ad060012c885949a6f0e602155
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-11 16:46:47 -0300

    feat: Implementa o usecase responsável por atualizar um cachorro

--- src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/cachorro/AtualizarCachorroCase.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/cachorro/AtualizarCachorroCase.java
@@ -1,4 +1,7 @@
 package com.GabrielTiziano.AuAuPI.core.usecases.cachorro;
 
+import com.GabrielTiziano.AuAuPI.core.entities.Cachorro;
+
 public interface AtualizarCachorroCase {
+    Cachorro execute(Long id, Cachorro cachorro);
 }
--- src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/cachorro/AtualizarCachorroCaseImpl.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/core/usecases/cachorro/AtualizarCachorroCaseImpl.java
@@ -1,4 +1,41 @@
 package com.GabrielTiziano.AuAuPI.core.usecases.cachorro;
 
+import com.GabrielTiziano.AuAuPI.core.entities.Cachorro;
+import com.GabrielTiziano.AuAuPI.core.gateway.CachorroGateway;
+import com.GabrielTiziano.AuAuPI.core.gateway.TutorGateway;
+
 public class AtualizarCachorroCaseImpl implements AtualizarCachorroCase{
+    private final CachorroGateway cachorroGateway;
+    private final TutorGateway tutorGateway;
+
+    public AtualizarCachorroCaseImpl(CachorroGateway cachorroGateway, TutorGateway tutorGateway) {
+        this.cachorroGateway = cachorroGateway;
+        this.tutorGateway = tutorGateway;
+    }
+
+    @Override
+    public Cachorro execute(Long id, Cachorro cachorro) {
+        if(!cachorroGateway.existsById(id)) {
+            throw new IllegalArgumentException("Não foi possível localizar um cachorro de id " + id + ".");
+        }
+
+        if (!tutorGateway.existsById(cachorro.idTutor())) {
+            throw new IllegalArgumentException("Tutor de id " + cachorro.idTutor() + " não encontrado.");
+        }
+
+        Cachorro cachorroASalvar = new Cachorro(
+                id,
+                cachorro.nome(),
+                cachorro.raca(),
+                cachorro.porte(),
+                cachorro.peso(),
+                cachorro.dataNascimento(),
+                cachorro.sexo(),
+                cachorro.castrado(),
+                cachorro.observacoesMedicas(),
+                cachorro.idTutor()
+        );
+
+        return cachorroGateway.save(cachorroASalvar);
+    }
 }
