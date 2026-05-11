f1db00dcdbef8ac59c8d9a2654294091b6806b88
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-11 17:29:01 -0300

    fix: Corrige o parametro de 'String' para enum no método findByPorte

--- src/main/java/com/GabrielTiziano/AuAuPI/infra/persistence/CachorroRepository.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/infra/persistence/CachorroRepository.java
@@ -1,9 +1,11 @@
 package com.GabrielTiziano.AuAuPI.infra.persistence;
 
+import com.GabrielTiziano.AuAuPI.core.enums.Porte;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CachorroRepository extends JpaRepository<CachorroEntity, Long> {
     List<CachorroEntity> findByTutorId(Long id);
+    List<CachorroEntity> findByPorte(Porte porte);
 }
