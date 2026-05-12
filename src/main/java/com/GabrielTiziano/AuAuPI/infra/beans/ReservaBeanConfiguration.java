912ead129c6d4acfbaf9876b66be1b7b08944bbd
GGhiaroni <gghiaronitiziano@gmail.com>
2026-05-12 11:38:16 -0300

    feat: Cria o Bean para dar contexto ao usecase de listar reservas de um cachorro

--- src/main/java/com/GabrielTiziano/AuAuPI/infra/beans/ReservaBeanConfiguration.java
+++ src/main/java/com/GabrielTiziano/AuAuPI/infra/beans/ReservaBeanConfiguration.java
@@ -4,6 +4,8 @@ import com.GabrielTiziano.AuAuPI.core.gateway.CachorroGateway;
 import com.GabrielTiziano.AuAuPI.core.gateway.ReservaGateway;
 import com.GabrielTiziano.AuAuPI.core.usecases.reserva.CriarReservaCase;
 import com.GabrielTiziano.AuAuPI.core.usecases.reserva.CriarReservaCaseImpl;
+import com.GabrielTiziano.AuAuPI.core.usecases.reserva.ListarReservasPorCachorroIdCase;
+import com.GabrielTiziano.AuAuPI.core.usecases.reserva.ListarReservasPorCachorroIdCaseImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -12,5 +14,10 @@ public class ReservaBeanConfiguration {
     @Bean
     public CriarReservaCase criarReservaCase(CachorroGateway cachorroGateway, ReservaGateway reservaGateway){
         return new CriarReservaCaseImpl(cachorroGateway, reservaGateway);
+    }
+
+    @Bean
+    public ListarReservasPorCachorroIdCase listarReservasPorCachorroIdCase(ReservaGateway reservaGateway, CachorroGateway cachorroGateway){
+        return new ListarReservasPorCachorroIdCaseImpl(reservaGateway, cachorroGateway);
     }
 }
